c725e6507fafb0e353411b1fb0c2c6472183c7b7
Adriela López <110007997+Adriela23@users.noreply.github.com>
2023-06-16 05:40:07 +0000

    programa terminado, pendiente agregar estilo

--- src/app.js
+++ src/app.js
@@ -2,10 +2,49 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+//Esta es la primera funcion que ejecuta el navegador despues de ejecutar el HTML
+window.onload = () => {
+  //Esto hace que el boton del HTML responda a un click
+  document.querySelector("#btnNewExcuse");
+  addEventListener("click", () => {
+    //Esto pone la excusa de forma aleatoria en el DOM, cada vez que se refresca la pagina
+    document.querySelector("#excuse").innerHTML = newExcuse();
+  });
+};
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+//Esta funcion genera excusas concatenando un elementto de cada arreglo
+let newExcuse = () => {
+  //arreglo que define el quien de la oracion
+  let who = [
+    "A racoon",
+    "A random guy",
+    "The neighbor’s cat",
+    "I couldn't help but"
+  ];
+
+  //arreglo que define el que de la oracion
+  let what = [
+    "stole my weekly report",
+    "threw my keys",
+    "hid my purse",
+    "ate a pinecone"
+  ];
+
+  //arreglo que define el cuando de la oracion
+  let when = [
+    "in the middle of the night",
+    "before I got inside the car",
+    "during lunch time",
+    "last weekend"
+  ];
+
+  //se usa Math.floor junto con Math.random para lanzar numeros aleatorios que sean enteros
+  //luego el numero aleatorio se multiplica por la longitud del arreglo
+  //y ese valor representa la palabra que se mostrara
+  let whoIndex = Math.floor(Math.random() * who.length);
+  let whatIndex = Math.floor(Math.random() * what.length);
+  let whenIndex = Math.floor(Math.random() * when.length);
+
+  //aqui la funcion devuelve la oracion concatenada
+  return who[whoIndex] + " " + what[whatIndex] + " " + when[whenIndex];
 };
